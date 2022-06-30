4fd101594838b500fb45db21e1b882877a6992f6
Raphael Kalinowski <raphael@myminicom.com>
2022-06-30 16:02:05 +0200

    clean main.js

--- website/js/main.js
+++ website/js/main.js
@@ -9,6 +9,14 @@ var uploader = new plupload.Uploader({
 
   init: {
 
+		// Display the files in the following div
+		FilesAdded: function (up, files) {
+			plupload.each(files, function (file) {
+				console.log("1- fileadded test", file);
+				document.getElementById('filelist').innerHTML += '<div id="' + file.id + '">' + file.name + ' (' + plupload.formatSize(file.size) + ') <b></b></div>';
+			});
+		},
+
 		PostInit: function () {
 			// Open the window to select and upload the files
 			document.getElementById('uploadfiles').onclick = function () {
@@ -18,23 +26,16 @@ var uploader = new plupload.Uploader({
 			};
 		},
 
-		// Display the files in the following div
-		FilesAdded: function (up, files) {
-			plupload.each(files, function (file) {
-				console.log("1- fileadded test", file);
-				document.getElementById('filelist').innerHTML += '<div id="' + file.id + '">' + file.name + ' (' + plupload.formatSize(file.size) + ') <b></b></div>';
-			});
-		},
-
 		// Display progress in console and DOM
 		UploadProgress: function (up, file) {
 			console.log("upload progress", file);
 			document.getElementById(file.id).getElementsByTagName('b')[0].innerHTML = '<span>' + file.percent + "%</span>";
 		},
 
-		// Handle errors
-		Error: function (up, err) {
-			document.getElementById('console').appendChild(document.createTextNode("\nError #" + err.code + ": " + err.message));
+		// Display info when each part/chunk is uploaded
+		ChunkUploaded: function (up, file, info) {
+			console.log("3-  Chunk uploaded: ", file);
+			console.log("3- chunk info", info)
 		},
 
 		// Display in console when file (when not chunked) are uploaded
@@ -47,10 +48,9 @@ var uploader = new plupload.Uploader({
 			}
 		},
 
-		// Display info when each part/chunk is uploaded
-		ChunkUploaded: function (up, file, info) {
-			console.log("3-  Chunk uploaded: ", file);
-			console.log("3- chunk info", info)
+		// Handle errors
+		Error: function (up, err) {
+			document.getElementById('console').appendChild(document.createTextNode("\nError #" + err.code + ": " + err.message));
 		}
 
 	}
