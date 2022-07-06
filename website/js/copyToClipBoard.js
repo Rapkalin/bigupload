fbaead5e0132404ef1350ba9ce9271bf0d4ac4a3
Raphael Kalinowski <raphael@myminicom.com>
2022-07-06 14:05:48 +0200

    fixed clipboard functionnality

--- website/js/copyToClipBoard.js
+++ website/js/copyToClipBoard.js
@@ -1,33 +1,17 @@
 function copyToClipboard() {
-    /* Get the text field */
-/*    var copyText = document.getElementById("downloadLink");
-    console.log("copyText", copyText);
-
-    /!* Select the text field *!/
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); /!* For mobile devices *!/
-
-    /!* Copy the text inside the text field *!/
-    navigator.clipboard.writeText(copyText.value);
-
-    /!* Alert the copied text *!/
-    alert("Copied the text: " + copyText.value);*/
-
     navigator.permissions.query({ name: 'clipboard-read' }).then(result => {
-    var copyText = document.getElementById("downloadLink");
-    console.log("copyText", copyText);
     console.log("Result", result);
         if (result.state === 'denied') {
-            alert("Woupsy it seems that you don't have the permission to do that!);
+            alert("Woupsy it seems that you don't have the permission to do that!");
         }
 
     // If permission to read the clipboard is granted or if the user will
     // be prompted to allow it, we proceed.
         if (result.state === 'granted' || result.state === 'prompt') {
-            navigator.clipboard.readText()
-                .then(copyText => {
-                    navigator.clipboard.writeText(copyText.value);
-                    alert("Copied the text: " + copyText.value);
+            var copyText = document.getElementById("downloadLink");
+            navigator.clipboard.writeText(copyText.value)
+                .then(text => {
+                    console.log("writeBoard: ", text);
                 })
                 .catch(err => {
                     console.error('Failed to read clipboard contents: ', err);
