f6ae97788ae75c98438eb84da4206c384a480bc7
Raphael Kalinowski <raphael@myminicom.com>
2022-07-12 14:19:27 +0200

    Edit  to /Users/r.kalinowski/.nvm/versions/node/v8.17.0/bin ./bin ./node_modules/.bin /Users/r.kalinowski/.pyenv/bin /Users/r.kalinowski/.rbenv/shims /Users/r.kalinowski/.rbenv/bin /Applications/MAMP/bin/php/php7.3.29/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/local/sbin

--- website/scripts/cron.php
+++ website/scripts/cron.php
@@ -4,14 +4,16 @@
 // 0 0 * * * php  /Users/r.kalinowski/Sites/bigupload/website/scripts/cron.php
 
 // Uncomment the right path: $pathServer if you are on prod env and $pathLocal if you are on local env.
+//Server : "/bigupload/website/app/uploads/";
+// Local : "website/app/uploads/";
 // $pathServer = "/bigupload/website/app/uploads/";
-$pathLocal = "website/app/uploads/";
+$path = "website/app/uploads/";
 
 // retrieve the files in the upload
-if (scandir("website/app/uploads/") > 0) {
-    foreach(array_diff(scandir("website/app/uploads/"), array('.', '..')) as $file) {
-        echo "Deleting: " . $pathLocal . $file . "\n";
-            if (unlink($pathLocal . $file)) {
+if (count(scandir($path)) > 0) {
+    foreach(array_diff(scandir($path), array('.', '..')) as $file) {
+        echo "Deleting: " . $path . $file . "\n";
+            if (unlink($path . $file)) {
                 echo "File deleted!" . "\n";
                 echo "------" . "\n" . "\n";
             } else {
