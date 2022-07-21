9f1b53ff5889226d0dd2340b909e107de5482ecf
Raphael Kalinowski <raphael@myminicom.com>
2022-07-21 17:40:52 +0200

    Edit path in cron.php

--- website/scripts/cron.php
+++ website/scripts/cron.php
@@ -1,12 +1,11 @@
 <?php
 
 // Setup the following cron to remove the files on you server once a day
-// 0 0 * * * php  /Users/r.kalinowski/Sites/bigupload/website/scripts/cron.php
+// 0 0 * * * php  ~/Users/r.kalinowski/Sites/bigupload/website/scripts/cron.php
 
 // Uncomment the right path: $pathServer if you are on prod env and $pathLocal if you are on local env.
-//Server : "/bigupload/website/app/uploads/";
+// Server : "/bigupload/website/app/uploads/";
 // Local : "website/app/uploads/";
-// $pathServer = "/bigupload/website/app/uploads/";
 $path = "website/app/uploads/";
 
 // retrieve the files in the upload
