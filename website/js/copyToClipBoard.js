b269e90be4db10dc23eca7e52f42af9b142286b6
Rapkalin <raphael@myminicom.com>
2024-04-15 00:21:22 +0200

    Fix brower check

--- website/js/copyToClipBoard.js
+++ website/js/copyToClipBoard.js
@@ -84,16 +84,26 @@ function fallbackCopyTextToClipboard(textToCopy) {
  * @returns {string}
  */
 function browserCheck () {
-    switch (true) {
-        case agent.indexOf("edge") > -1: return "MS Edge (EdgeHtml)";
-        case agent.indexOf("edg") > -1: return "MS Edge Chromium";
-        case agent.indexOf("opr") > -1 && !!window.opr: return "opera";
-        case agent.indexOf("chrome") > -1 && !!window.chrome: return "chrome";
-        case agent.indexOf("trident") > -1: return "Internet Explorer";
-        case agent.indexOf("firefox") > -1: return "firefox";
-        case agent.indexOf("safari") > -1: return "safari";
-        default: return "other";
-    }
+    return (function (agent) {
+        switch (true) {
+            case agent.indexOf("edge") > -1:
+                return "MS Edge (EdgeHtml)";
+            case agent.indexOf("edg") > -1:
+                return "MS Edge Chromium";
+            case agent.indexOf("opr") > -1 && !!window.opr:
+                return "opera";
+            case agent.indexOf("chrome") > -1 && !!window.chrome:
+                return "chrome";
+            case agent.indexOf("trident") > -1:
+                return "Internet Explorer";
+            case agent.indexOf("firefox") > -1:
+                return "firefox";
+            case agent.indexOf("safari") > -1:
+                return "safari";
+            default:
+                return "other";
+        }
+    });
 }
 
 /**
