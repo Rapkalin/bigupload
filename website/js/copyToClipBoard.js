42d719f53f227c855285c740bf20ff515e70ea3c
Rapkalin <raphael@myminicom.com>
2024-04-14 22:33:40 +0200

    Fix clipboard Firefox

--- website/js/copyToClipBoard.js
+++ website/js/copyToClipBoard.js
@@ -1,22 +1,48 @@
-function copyToClipboard() {
-    navigator.permissions.query({ name: 'clipboard-read' }).then(result => {
-    console.log("Result", result);
-        if (result.state === 'denied') {
-            alert("Woupsy! It seems that you did not authorize your navigator to clipboarb. Please check your settings");
-        }
+const permissionsCheck = async () => {
+    let read = await navigator.permissions.query({name: 'clipboard-read'})
+    let write = await navigator.permissions.query({name: 'clipboard-write'})
+    return write.state === 'granted' && read.state !== 'denied'
+}
 
-    // If permission to read the clipboard is granted or if the user will
-    // be prompted to allow it, we proceed.
-        if (result.state === 'granted' || result.state === 'prompt') {
-            var copyText = document.getElementById("downloadLink");
-            navigator.clipboard.writeText(copyText.value)
-                .then(text => {
-                    console.log("writeBoard: ", text);
-                })
-                .catch(err => {
-                    console.error('Failed to read clipboard contents: ', err);
-                    alert("Seems like there is an unexpected error: " + err);
-                });
+function copyToClipboard() {
+    try {
+        navigator.permissions.query({ name: 'clipboard-read' })
+            .then(result => {
+                console.info("Clipboard-read permission result successful: " + result);
+            if (result.state === 'denied') {
+                console.error("Clipboard-read permission result denied: " + result);
+                alert("Copied didn't work. Please authorize your navigator to clipboard");
+            }
+
+            /*
+            If permission to read the clipboard is granted or if the user will
+            be prompted to allow it, we proceed.
+             */
+            if (result.state !== 'denied') {
+                var copyText = document.getElementById("downloadLink");
+                navigator.clipboard.writeText(copyText.value)
+                    .then(text => {
+                        console.info("Text successfully copied: " + text);
+                    })
+                    .catch(err => {
+                        console.error('Failed to read clipboard contents: ' + err);
+                    });
+            }
+        })
+    } catch (e) {
+        console.error('Error with clipboard-read: ' + e)
+    }
+
+}
+
+const initClipboard = async () => {
+    await permissionsCheck()
+        .then(allowed => {
+        if (allowed){
+            copyToClipboard()
+        } else {
+            console.error('Check clipboard permission failed!')
         }
     })
+
 }
