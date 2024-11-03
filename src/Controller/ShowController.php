dbc16b3f1ba30339b44ed5b2e3152c47f0b38877
rapkalin <raphael@myminicom.com>
2024-11-03 15:56:55 +0100

    Mock show page

--- src/Controller/ShowController.php
+++ src/Controller/ShowController.php
@@ -11,8 +11,16 @@ class ShowController extends BaseController
     #[Route('/downloads/show', name: 'downloads.show')]
     public function index(Request $request): Response
     {
+        $item = [
+            'download_url' => 'https://google.com',
+            'title' => 'Ceci est le titre du fichier',
+            'expiration_date' => '12 janvier 2000',
+            'expiration_time' => '3 jours',
+            'size' => '18ko',
+            'extension' => 'pdf',
+        ];
         return $this->render('show/index.html.twig', [
-            'downloadUrl' => 'ShowController',
+            'item' => $item,
         ]);
     }
 }
