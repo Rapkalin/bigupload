1ad799266c2e7cd483ba821eede01dbce094d011
rapkalin <raphael@myminicom.com>
2024-12-05 18:27:53 +0100

    Button animation done

--- assets/scripts/button_animation.js
+++ assets/scripts/button_animation.js
@@ -1,9 +1,11 @@
-const button = document.querySelector(".btn-bglpd");
+const button = document.getElementById("copyLinkButton");
 button.addEventListener("click", addClassAnimation, false);
 
 function addClassAnimation(e) {
-    console.log('HERE IS THE EVENT:::', e);
     e.preventDefault();
     button.classList.add("animate");
 
+    setTimeout(() => {
+        button.classList.remove("animate");
+    }, 500) // 1s = 1000ms
 }
