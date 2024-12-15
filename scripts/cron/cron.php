03c33a2db9d6d8270e5a65db9b2f18e50d91b83b
rapkalin <raphael@myminicom.com>
2024-12-15 21:08:33 +0100

    Suppr file from DB if unlinked via cron

--- scripts/cron/cron.php
+++ scripts/cron/cron.php
@@ -5,15 +5,17 @@
 // 0 * * * * /Applications/MAMP/bin/php/php8.1.13/bin/php /Users/r.kalinowski/Sites/bigupload/scripts/cron/cron.php >> /Users/r.kalinowski/Sites/bigupload/scripts/cron/$(date +\%Y-\%m-\%d)_debug_cron.log
 // if log error only 2>&1: https://stackoverflow.com/questions/818255/what-does-21-mean
 
-connectDatabase();
+include(__DIR__ . '/../../vendor/autoload.php');
+use Symfony\Component\Dotenv\Dotenv;
+loadDotEnv();
 
 $serverPath = __DIR__ . "/../../public/uploads/";
 if (count(scandir($serverPath)) > 0) { // check if there are files in the upload directory
     $arrayFiles = listFilesToDelete($serverPath); // Check if there are files to be deleted in the directory
-    if (count($arrayFiles)) {
+    if (count($arrayFiles) && $pdo = connectDatabase()) {
         foreach($arrayFiles as $file) {
             if (fileCanBeDeleted($file, $serverPath)) {
-                deleteFile($file, $serverPath);
+                deleteFile($file, $serverPath, $pdo);
             } else {
                 echo "    DELETE PROCESS STOPPED    " . "\n";
                 echo "Why you ask? Well, this file is too young to die!!" . "\n";
@@ -30,7 +32,8 @@ if (count(scandir($serverPath)) > 0) { // check if there are files in the upload
 
 function fileCanBeDeleted(string $file, string $serverPath) : bool
 {
-    $extraTime = ' + 6 day'; // How long we let a file on the server +1 current day
+//    $extraTime = ' + 6 day'; // How long we let a file on the server +1 current day
+    $extraTime = ' + 1 minute'; // How long we let a file on the server +1 current day
     $filePath = $serverPath . $file;
     $fileCreatedAt = date("F d Y H:i:s.", filectime($filePath));
     $fileCreatedAtExtraTime = date("F d Y H:i:s.", strtotime($fileCreatedAt . $extraTime));
@@ -55,11 +58,12 @@ function listFilesToDelete(string $serverPath) : array
     return $filesToDelete;
 }
 
-function deleteFile(string $file, string $serverPath) : void
+function deleteFile(string $file, string $serverPath, PDO $pdo) : void
 {
     echo "     DELETING IN PROGRESS     " . "\n";
     echo "Deleting: " . $file . "\n" . "\n";
     if (unlink($serverPath . $file)) {
+        deleteFileFromDatabase($pdo, $file);
         echo "\n";
         echo "File deleted: $file!" . "\n";
         echo "\n" . "\n";
@@ -69,11 +73,30 @@ function deleteFile(string $file, string $serverPath) : void
     }
 }
 
-function connectDatabase() : void
+function connectDatabase() : PDO|bool
 {
-    $pdo = new PDO("mysql:host=" . $_ENV['DB_HOST'] . ";dbname=" . $_ENV['DB_NAME'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
-    $pdo->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, false);
+    try {
+        $pdo = new PDO("mysql:host=" . $_ENV['DB_HOST'] . ";dbname=" . $_ENV['DB_NAME'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
+        $pdo->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, false);
+        return $pdo;
+    } catch (PDOException $e) {
+        echo 'connectDatabase ERROR: ' . $e->getMessage() . PHP_EOL;
+        return false;
+    }
+}
 
-    $unbufferedResult = $pdo->query("SELECT title FROM items WHERE title=`dox_test_lourd.pdf`");
-    // dd('DB test', $unbufferedResult);
+function loadDotEnv() : void
+{
+    try {
+        $dotenv = new Dotenv();
+        $dotenv->load(__DIR__.'/../../.env');
+    } catch (Exception $e) {
+        echo 'loadDotEnv ERROR: ' . $e->getMessage() . PHP_EOL;
+    }
+}
+
+function deleteFileFromDatabase(PDO $pdo, $file) : void
+{
+    $unbufferedResult = $pdo->prepare('DELETE FROM items WHERE zip_name = ?');
+    $unbufferedResult->execute([$file]);
 }
