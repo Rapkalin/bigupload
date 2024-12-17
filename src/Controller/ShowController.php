1ed1f4ee7f768552227211172487cf18323b9cab
rapkalin <raphael@myminicom.com>
2024-12-17 16:20:52 +0100

    404 - add page file not found

--- src/Controller/ShowController.php
+++ src/Controller/ShowController.php
@@ -13,6 +13,14 @@ class ShowController extends BaseController
     public function index(Request $request, ItemRepository $itemRepository): Response
     {
         $item = $itemRepository->findOneBy(['show_id' => $request->attributes->get('showId')]);
+
+        if (!$item) {
+             throw $this->createNotFoundException(
+                 'It seems that the file you are looking for doesn\'t exist. <br>
+                 It might have been deleted.'
+             );
+        }
+
         return $this->render('show/index.html.twig', [
             'item' => $item->formatData(),
         ]);
