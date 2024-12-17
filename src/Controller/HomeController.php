82118cef7b29c51e3c5025052060ee92ac2292f2
rapkalin <raphael@myminicom.com>
2024-12-17 18:08:11 +0100

    Add counter to homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,14 +2,27 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
+use Doctrine\DBAL\Connection;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends BaseController
 {
+
+    public function __construct(Connection $connection) {
+        $this->connection = $connection;
+    }
+
     #[Route('/', name: 'home.index')]
     public function index() : Response {
-        return $this->render('home/index.html.twig', []);
+        return $this->render('home/index.html.twig', ['counter' => $this->getCounter()]);
+    }
+
+    private function getCounter(): int
+    {
+        $sql = 'SELECT * FROM count_downloads WHERE id = 1';
+        $stmt = $this->connection->prepare($sql);
+        $result = $stmt->executeQuery()->fetchAssociative();
+        return $result['uploaded_files'];
     }
 }
